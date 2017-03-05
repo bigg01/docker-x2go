d1b653e8fef9e14d4b574a75d431118252dce5bb
bigg01 <o.guggenbuehl@gmail.com>
2017-03-05 21:30:20 +0100

    go x2go

--- Dockerfile
+++ Dockerfile
@@ -15,17 +15,8 @@ RUN yum -y groupinstall Xfce --setopt=tsflags=nodocs
 
 ADD ./start.sh /start.sh
 
-RUN sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config && \
-    sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config && \
-    sed -i "s/#Port.*/Port 22/g" /etc/ssh/sshd_config && \
-    sed -i "s/*.UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config && \
-    mkdir /var/run/sshd && \
-    ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' && \ 
-    ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N '' && \
-    ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N '' && \
-    yum clean all
-
 RUN chmod +x /start.sh && ./start.sh
 EXPOSE 22
 
-CMD    ["/usr/sbin/sshd", "-D"]
+#CMD    ["/usr/sbin/sshd", "-D"]
+CMD    ["/usr/sbin/sshd", "-f", "/home/user/ssh/sshd_config, "-D"]
--- start.sh
+++ start.sh
@@ -2,10 +2,27 @@
 
 __create_user() {
 # Create a user to SSH into as.
-useradd user
+useradd user -m
 SSH_USERPASS=newpass
 echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin user)
 echo ssh user password: $SSH_USERPASS
+cd /home/user
+mkdir -p etc/ssh var/run
+cp /etc/ssh/sshd_config etc/ssh/sshd_config
+
+sed -i "s/UsePAM.*/UsePAM no/g" etc/ssh/sshd_config && \
+sed -i "s/#PermitRootLogin/PermitRootLogin/g" etc/ssh/sshd_config && \
+sed -i "s/#Port.*/Port 2022/g" etc/ssh/sshd_config && \
+sed -i "s/*.UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" etc/ssh/sshd_config && \
+sed -i "s/*.PidFile.*/PidFile \/home\/user\/var\/run\/sshd.pid/g" etc/ssh/sshd_config && \
+sed -i "s/*.HostKey /etc/ssh/ssh_host_rsa_key/\/home\/user\/etc\/ssh_host_rsa_key/g" etc/ssh/sshd_config && \
+sed -i "s/*.HostKey /etc/ssh/ssh_host_ecdsa_key/\/home\/user\/etc\/ssh_host_ecdsa_key/g" etc/ssh/sshd_config && \
+sed -i "s/*.HostKey /etc/ssh/ssh_host_ed25519_key/\/home\/user\/etc\/ssh_host_ed25519_key/g" etc/ssh/sshd_config && \
+mkdir /var/run/sshd && \
+ssh-keygen -t rsa -f /home/user/etc/ssh/ssh_host_rsa_key -N '' && \
+ssh-keygen -t ecdsa -f /home/user/ssh/ssh_host_ecdsa_key -N '' && \
+ssh-keygen -t ed25519 -f /home/user/ssh/ssh_host_ed25519_key -N '' && \
+yum clean all
 }
 
 # Call all functions
